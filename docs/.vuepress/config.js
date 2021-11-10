a8b6eda9e4fba4f4e20f4ce3e0bb77b638e4a325
DESKTOP-RRTN9G3\zhang <316555130@qq.com>
2021-11-10 14:28:13 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  title: 'zj-web-ui',
+  base: '/zj-web-ui',
   themeConfig: {
+    nav: [{
+      text: 'Home', link: '/'
+    },{
+      text: 'Github', link: 'https://zhangjiejack.github.io/zj-web-ui/'
+    },{
+      text: 'Npm', link: 'https://www.npmjs.com/package/zj-web-ui'
+    }],
     sidebar: [
       '/',
       '/componentsDocs/card'
